482874d8842ff9742f206539d030cc3ec3e18f0e
Md-Tohin <mdtohin8585@gmail.com>
2025-01-26 19:56:52 +0600

    School Dashboard design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import School from './school/School'
+import Testing from './Testing'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Testing />}></Route>
         {/* SCHOOL ROUTE */}
         <Route path="school" element={<School />}>
 
---
+++ frontend/src/Testing.jsx
@@ -0,0 +1,11 @@
+
+
+const Testing = () => {
+  return (
+    <div>
+      Multi School Management System Software
+    </div>
+  )
+}
+
+export default Testing
